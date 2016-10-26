8fd6569ffdee90b2ece29bc0b659131631bb5afa
EricKuang <297466058@qq.com>
2016-10-26 18:47:22 +0800

    ...

---
+++ wetouter.js
@@ -0,0 +1,56 @@
+export default function frontRouter(defaultHash) {
+    if (!!defaultHash) {
+            location.hash = defaultHash;
+	        }
+		};
+
+		frontRouter.prototype.use = function(hashTo, callback) {
+
+		    var slicer = hashTo.slice('1');
+		        if (/\:/gm.test(slicer)) {
+			        location.hash = slicer.replace(/\:/gm, '/');
+				    } else {
+				            location.hash = slicer;
+					        };
+
+						    callback && callback();
+						    };
+
+						    frontRouter.prototype.haser = function() {
+
+						    }
+
+						    frontRouter.prototype.init = function() {
+						        window.addEventListener('hashchange', function() {
+							        if (document.location == '#hash') {}
+								    }, false);
+								    };
+
+								    frontRouter.prototype.map = function(object) {
+								        window.addEventListener('hashchange', function() {
+									        var _hash;
+
+										        if (location.hash == "") {
+											            _hash = '/'
+												            } else {
+													                _hash = location.hash.replace('#', '/');
+															        }
+
+																        var _reg = new RegExp(_hash);
+																	        for (var i in object) {
+																		            if (_reg.test(i)) {
+																			                    object[i]();
+																					                } 
+																							            // else if(new RegExp('error').test(i) || 0) {
+																								                //     object['error']();
+																										            // }
+																											            }
+																												        }, false);
+																													};
+
+																													// router.map({
+																													//     '/nihao': function(){console.log('hello')},
+																													//     '/john': function(){console.log('wohao')}
+
+																													// })
+
